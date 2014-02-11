a24f882316d87c7644872c9db418ba6929a971dc
Brian Kelly <polymonic@gmail.com>
2014-02-10 19:14:52 -0500

    Pull Twitter keys out of code and into environment variables. Pull from one of Twitter lists as well.

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,20 +1,20 @@
 require 'twitter'
+require 'dotenv'
 
+Dotenv.load
 
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.oauth_token = 'YOUR_OAUTH_TOKEN'
-  config.oauth_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.access_token_secret = ENV['TWITTER_OAUTH_SECRET']
 end
-
-search_term = URI::encode('#todayilearned')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
-    tweets = twitter.search("#{search_term}")
+    tweets = twitter.list_timeline("spilth", "minimalism")
 
     if tweets
       tweets = tweets.map do |tweet|
